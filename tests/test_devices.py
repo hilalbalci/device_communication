a21689cd82bd849e53a42905985a3947a420e2b3
Hilal Balcı <hilalbalci@myhost.local>
2024-10-13 13:19:25 +0300

    Edit tests

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -10,10 +10,7 @@ from app.database import Base, get_db
 from app.main import app
 from app.models import Device, DeviceLocation
 
-client = TestClient(app)
-
 SQLITE_DATABASE_URL = "sqlite:///./test_db.db"
-
 engine = create_engine(
     SQLITE_DATABASE_URL,
     connect_args={
@@ -30,9 +27,6 @@ def override_get_db():
     database.close()
 
 
-app.dependency_overrides[get_db] = override_get_db
-
-
 def add_test_data(db):
     device1 = Device(name="Test Device 1", description="This is a test device.")
     device2 = Device(name="Test Device 2", description="This is another test device.")
@@ -41,9 +35,9 @@ def add_test_data(db):
     db.add(device2)
     db.commit()
 
-    location1 = DeviceLocation(device_id=device1.id, latitude=42.7128, longitude=-73.0060)
-    location2 = DeviceLocation(device_id=device1.id, latitude=43.7128, longitude=-74.0060)
-    location3 = DeviceLocation(device_id=device2.id, latitude=44.7128, longitude=-75.0060)
+    location1 = DeviceLocation(device_id=device1.id, latitude=1, longitude=4)
+    location2 = DeviceLocation(device_id=device1.id, latitude=2, longitude=5)
+    location3 = DeviceLocation(device_id=device2.id, latitude=3, longitude=6)
 
     db.add(location1)
     db.add(location2)
@@ -57,21 +51,28 @@ def test_db():
     db = TestingSessionLocal()
     add_test_data(db)
     db.close()
-    yield
-    Base.metadata.drop_all(bind=engine)
+    yield db
+    # Base.metadata.drop_all(bind=engine)
 
 
-def test_create_device():
+@pytest.fixture()
+def test_client():
+    app.dependency_overrides[get_db] = override_get_db
+    client = TestClient(app)
+    yield client
+
+
+def test_create_device(test_client):
     lowercase_str = uuid.uuid4().hex
     query = f"""mutation MyMutation {{createDevice(name: "{lowercase_str}") {{ id name description }}}}"""
-    response = client.post("/graphql", json={"query": query})
+    response = test_client.post("/graphql", json={"query": query})
     assert response.status_code == 200
     assert response.json()["data"]["createDevice"]["name"] == lowercase_str
 
 
-def test_list_locations_by_device(test_db):
-    db = TestingSessionLocal()
-    locations = get_last_location_of_every_device(db)
+def test_list_locations(test_db, test_client):
+    locations = test_db.query(DeviceLocation).join(Device, Device.id == DeviceLocation.device_id). \
+        filter(Device.is_deleted == False)
     query = """query MyQuery {
             listLocations {
             deviceId
@@ -80,8 +81,17 @@ def test_list_locations_by_device(test_db):
             timestamp
             }
             }"""
-    response = client.post("/graphql", json={"query": query})
+    response = test_client.post("/graphql", json={"query": query})
     assert response.status_code == 200
-    assert len(response.json()["data"]["listLocations"]) == len(locations)
+    assert len(response.json()["data"]["listLocations"]) == locations.count()
 
 
+def test_list_locations_by_device(test_db, test_client):
+    locations = test_db.query(DeviceLocation).join(Device, Device.id == DeviceLocation.device_id).\
+        filter(Device.is_deleted == False)
+    device_id = locations.first().device_id
+    device_locations = locations.filter(Device.id == device_id)
+    query = f"""query MyQuery {{listLocationByDevice(deviceId: {device_id}) {{deviceId}}}}"""
+    response = test_client.post("/graphql", json={"query": query})
+    assert response.status_code == 200
+    assert len(response.json()["data"]["listLocationByDevice"]) == device_locations.count()
